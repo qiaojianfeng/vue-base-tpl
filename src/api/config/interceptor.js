09f85720e4b8c66c68b92618c2c7e50f2127a4db
QiaoJianFeng <845882161@qq.com>
2019-08-07 17:30:00 +0800

    Update interceptor.js

--- src/api/config/interceptor.js
+++ src/api/config/interceptor.js
@@ -1,14 +1,17 @@
 // import { BASE_URL } from './config';
 
 export const requestInterceptor = function(config) {
+//   请求正确拦截
   return config;
 };
 
 export const requestInterceptorError = function(error) {
+//   请求错误拦截
   return Promise.reject(error);
 };
 
 export const responseInterceptor = function(response) {
+//   响应正确拦截
   // const data = response.data || { code: undefined };
   // const code = data.code;
   // const pageURL = window.location.href;
@@ -16,5 +19,6 @@ export const responseInterceptor = function(response) {
   return response;
 };
 export const responseInterceptorError = function(error) {
+//   响应错误拦截
   return Promise.reject(error);
 };
